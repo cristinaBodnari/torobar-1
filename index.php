9691236b9e372b599394aff7d961f3ff7153f0b8
Rimvydas Zilinskas <rilinskas@yahoo.com>
2018-11-20 16:45:23 +0100

    title

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+  // put your title for the page here, otherwise it's gonna be the default "TOROS"
+  $title = "Index";
   require_once("shared/header.php");
 ?>
   <!-- Insert code here -->
--- shared/header.php
+++ shared/header.php
@@ -1,15 +1,20 @@
+<?php 
+    if(defined($title)){
+      $title="TORO bar";
+    }
+?>
 <!doctype html>
 
 <html lang="en">
 <head>
   <meta charset="utf-8">
 
-  <title>The HTML5 Herald</title>
+  <title><?php echo $title?></title>
   <meta name="description" content="">
   <meta name="author" content="The best group">
 
   <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 </head>
 
