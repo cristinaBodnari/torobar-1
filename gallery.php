98740af2c466e3ed5db8e787ec5355f2315e4ae0
Rimvydas Zilinskas <rilinskas@yahoo.com>
2018-11-20 16:47:32 +0100

    edit

--- gallery.php
+++ gallery.php
@@ -1,20 +1,9 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-
-  <title>The HTML5 Herald</title>
-  <meta name="description" content="">
-  <meta name="author" content="The best group">
-
-  <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-</head>
-
-<body>
-  
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+  // put your title for the page here, otherwise it's gonna be the default "TOROS"
+  $title = "Galerry";
+  require_once("shared/header.php");
+?>
+  <!-- Insert code here -->
+<?php 
+  require_once("shared/footer.php");
+?>
