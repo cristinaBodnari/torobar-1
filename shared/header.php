77a28b537f724542741186fde7a82c41c70e7b1a
“Princess <anto6519@stud.kea.dk>
2018-11-20 16:51:41 +0100

    updated header file with new menu bar and css file

--- shared/header.php
+++ shared/header.php
@@ -1,21 +1,29 @@
-<?php 
-    if(defined($title)){
-      $title="TORO bar";
-    }
-?>
 <!doctype html>
 
 <html lang="en">
 <head>
   <meta charset="utf-8">
 
-  <title><?php echo $title?></title>
+  <title>The HTML5 Herald</title>
   <meta name="description" content="">
   <meta name="author" content="The best group">
 
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 </head>
 
 <body>
+
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#menu">Menu</a>
+  <a href="#about">About</a>
+  <a href="#events">Events</a>
+  <a href="#gallery">Gallery</a>
+  <a href="#contact">Contact</a>
+</div>
+
+<div class="main">
+  <p>Some text some text some text some text..</p>
+</div>
